815fa31a3b3a642ca8cfeacc47f19a4ede811e09
Drazen Drinic <drazendrinic@hotmail.com>
2022-09-24 17:41:47 -0400

    Welcome method

--- src/com/lockersPvtLtd/application/VirtualKeyForYourRepositories.java
+++ src/com/lockersPvtLtd/application/VirtualKeyForYourRepositories.java
@@ -1,10 +1,35 @@
 package com.lockersPvtLtd.application;
 
+
+import java.sql.SQLOutput;
+import java.text.Format;
+
 public class VirtualKeyForYourRepositories {
 
     public static void main(String[] args) {
 
+        Welcome();
+
     }
+
+    private static void Welcome(){
+        String firstLine = "-----------------------------------***-----------------------------------"; // Length is 65 in total
+        String developerLine = "Application Developer : Drazen Drinic";
+        String applicationLine = "Application Name: Virtual Key For Your Repositories Application";
+        String lastLine = "-----------------------------------***-----------------------------------";
+
+        System.out.printf("\n\n+%"+firstLine.length()+"s+ \n", firstLine);
+        System.out.printf("| %-"+(firstLine.length()-2)+"s |\n", developerLine);
+        System.out.printf("| %-"+(firstLine.length()-2)+"s |\n", applicationLine);
+        System.out.printf("+%"+firstLine.length()+"s+ \n\n", lastLine);
+    }
+
+/*    public static String CreateLine(String line, String firstLineForLength){
+
+
+
+        return
+    }*/
 
 
 }
